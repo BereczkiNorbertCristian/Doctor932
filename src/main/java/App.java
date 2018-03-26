ef862270e65c381fc9105b2d0271978381c4116a
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2018-03-26 23:37:40 +0300

    Added tets

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,13 @@ import controller.DoctorController;
 
 public class App {
 
-	public static void main(String[] args) {
-		String patients = "FilePatients.txt";
-		String consultations = "FileConsultations.txt";
-		Repository repo = new Repository(patients, consultations);
-		DoctorController ctrl = new DoctorController(repo);
-		
-		DoctorUI console = new DoctorUI(ctrl);
-		console.Run();
-	}
+    public static void main(String[] args) {
+        String patients = "FilePatients.txt";
+        String consultations = "FileConsultations.txt";
+        Repository repo = new Repository(patients, consultations);
+        DoctorController ctrl = new DoctorController(repo);
+
+        DoctorUI console = new DoctorUI(ctrl);
+        console.Run();
+    }
 }
--- src/test/java/controller/DoctorControllerTest.java
+++ src/test/java/controller/DoctorControllerTest.java
@@ -9,7 +9,7 @@ import repository.Repository;
 import static org.junit.Assert.*;
 
 /**
- * Created by Vali on 14.03.2018.
+ * Created by bnorbert on 14.03.2018.
  */
 public class DoctorControllerTest {
 
@@ -18,19 +18,44 @@ public class DoctorControllerTest {
 
     @Before
     public void setUp() {
-        repository = new Repository("FilePatients.txt","FileConsultations.txt");
+        repository = new Repository("FilePatients.txt", "FileConsultations.txt");
         controller = new DoctorController(repository);
     }
 
     @Test
     public void addPatient() throws Exception {
         int n = controller.getPatientList().size();
-        controller.addPatient(new Patient("2222222222222","John","ooo"));
-        assertEquals(controller.getPatientList().size(),n+1);
+        controller.addPatient(new Patient("2222222222222", "John", "ooo"));
+        assertEquals(n + 1, controller.getPatientList().size());
     }
 
     @Test(expected = PatientException.class)
-    public void addPatientException() throws PatientException {
-        controller.addPatient(new Patient(null,null,null));
+    public void addPatientNullSSN() throws PatientException {
+        controller.addPatient(new Patient(null, "John", "ooo"));
+    }
+
+    @Test(expected = PatientException.class)
+    public void addPatientInvalidNumberOfCharactersSSN() throws PatientException {
+        controller.addPatient(new Patient("4444", "John", "ooo"));
+    }
+
+    @Test(expected = PatientException.class)
+    public void addPatientInvalidFormatSSN() throws PatientException {
+        controller.addPatient(new Patient("ds22222", "John", "ooo"));
+    }
+
+    @Test(expected = PatientException.class)
+    public void addPatientInvalidDigitsName() throws PatientException {
+        controller.addPatient(new Patient("2222222222222", "John1", "ooo"));
+    }
+
+    @Test(expected = PatientException.class)
+    public void addPatientNullName() throws PatientException {
+        controller.addPatient(new Patient("2222222222222", null, "ooo"));
+    }
+
+    @Test(expected = PatientException.class)
+    public void addPatientNullAddress() throws PatientException {
+        controller.addPatient(new Patient("2222222222222", "John", null));
     }
 }
