fbcdc99db0fc10377bff964905370a102914b6bc
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2018-04-18 09:09:50 +0300

    added more asserts

--- src/test/java/controller/DoctorControllerTopDownTest.java
+++ src/test/java/controller/DoctorControllerTopDownTest.java
@@ -32,19 +32,29 @@ public class DoctorControllerTopDownTest {
 
     @Test
     public void reqA() throws PatientException {
+        int before = controller.getPatientList().size();
         controller.addPatient(new Patient("8888888888888","Ron","Liverpool"));
+        assertEquals(before+1,controller.getPatientList().size());
     }
 
     @Test
     public void reqAB() throws ConsultationException, PatientException {
+        int beforePatients = controller.getPatientList().size();
+        int beforeConsultations = controller.getConsultationList().size();
         controller.addPatient(new Patient("8888888888888","Ron","Liverpool"));
         controller.addConsultation("32","8888888888888","dragon-pox",Arrays.asList("dittany"),"2018-03-03");
+        assertEquals(beforePatients+1,controller.getPatientList().size());
+        assertEquals(beforeConsultations+1,controller.getConsultationList().size());
     }
 
     @Test
     public void reqABC() throws PatientException, ConsultationException {
+        int beforePatients = controller.getPatientList().size();
+        int beforeConsultations = controller.getConsultationList().size();
         controller.addPatient(new Patient("8888888888888","Ron","Liverpool"));
         controller.addConsultation("32","8888888888888","dragon-pox",Arrays.asList("dittany"),"2018-03-03");
+        assertEquals(beforePatients+1,controller.getPatientList().size());
+        assertEquals(beforeConsultations+1,controller.getConsultationList().size());
         assertEquals("Ron",controller.getPatientsWithDisease("dragon-pox").get(0).getName());
     }
 
